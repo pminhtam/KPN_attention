ecb8216fa14bbe283943743a05fc7752625e8fe8
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-12-10 16:32:10 +0700

    add LossAnneal_i and synthtic data loder for att_kpn_dgf

--- utils/data_provider_DGF_synthetic.py
+++ utils/data_provider_DGF_synthetic.py
@@ -53,13 +53,15 @@ class SingleLoader_DGF_synth(data.Dataset):
         rand_affine = torch.rand(1)[0]
         angle = torch.randint(low= -20,high=20,size=(1,))[0]
         image_gt = Image.open(self.gt_path[index]).convert('RGB')
-        nw = image_gt.size(-1) - self.image_size
-        nh = image_gt.size(-2) - self.image_size
-        while nw > 0 and nh > 0:
+        # print(image_gt.size)
+        nw = image_gt.size[-1] - self.image_size
+        nh = image_gt.size[-2] - self.image_size
+        while nw < 0 and nh < 0:
             del self.gt_path[index]
+            print("del  : ",index)
             image_gt = Image.open(self.gt_path[index]).convert('RGB')
-            nw = image_gt.size(-1) - self.image_size
-            nh = image_gt.size(-2) - self.image_size
+            nw = image_gt.size[-1] - self.image_size
+            nh = image_gt.size[-2] - self.image_size
         image_gt = random_flip(image_gt,rand_hflip,rand_vflip)
         image_gt = random_rotate(image_gt,rand_affine,angle)
 
