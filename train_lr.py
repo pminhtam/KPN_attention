a1169f224d9ab81bdc318cf59011d2bbb464fe0e
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-12-15 14:27:38 +0700

    add test in  train_lr.py

--- train_lr.py
+++ train_lr.py
@@ -17,7 +17,9 @@ from utils.loss import LossBasic,WaveletLoss,tv_loss,LossAnneal_i
 from model.KPN import KPN
 from model.Att_KPN import Att_KPN, Att_KPN_Wavelet
 from model.Att_Weight_KPN import Att_Weight_KPN
-
+import scipy.io
+from collections import OrderedDict
+from test_NonKPN_DGF_mat import load_data
 def train(num_workers, cuda, restart_train, mGPU):
     # torch.set_num_threads(num_threads)
 
@@ -274,30 +276,8 @@ def train(num_workers, cuda, restart_train, mGPU):
 
 def eval(args):
     color = True
-    print('Eval Process......')
-    burst_length = 16
-
-    checkpoint_dir = "checkpointa/" + args.checkpoint
-    if not os.path.exists(checkpoint_dir) or len(os.listdir(checkpoint_dir)) == 0:
-        print('There is no any checkpoint file in path:{}'.format(checkpoint_dir))
-    # the path for saving eval images
-    eval_dir = "eval_img"
-    if not os.path.exists(eval_dir):
-        os.mkdir(eval_dir)
-    files = os.listdir(eval_dir)
-    for f in files:
-        os.remove(os.path.join(eval_dir, f))
-
-    # dataset and dataloader
-    data_set = SingleLoader_DGF(noise_dir=args.noise_dir,gt_dir=args.gt_dir,image_size=args.image_size)
-    data_loader = DataLoader(
-        data_set,
-        batch_size=1,
-        shuffle=False,
-        num_workers=args.num_workers
-    )
-
-    # model here
+    burst_length = args.burst_length
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if args.model_type == "attKPN":
         model = Att_KPN(
             color=color,
@@ -310,10 +290,10 @@ def eval(args):
             upMode="bilinear",
             core_bias=False
         )
-    elif args.model_type == "attWKPN":
-        model = Att_Weight_KPN(
+    elif args.model_type == "attKPN_Wave":
+        model = Att_KPN_Wavelet(
             color=color,
-            burst_length=burst_length,
+            burst_length=1,
             blind_est=True,
             kernel_size=[5],
             sep_conv=False,
@@ -322,10 +302,22 @@ def eval(args):
             upMode="bilinear",
             core_bias=False
         )
-    elif args.model_type == 'KPN':
+    elif args.model_type == "attWKPN":
+        model = Att_Weight_KPN(
+            color=color,
+            burst_length=1,
+            blind_est=True,
+            kernel_size=[5],
+            sep_conv=False,
+            channel_att=True,
+            spatial_att=True,
+            upMode="bilinear",
+            core_bias=False
+        )
+    elif args.model_type == "KPN":
         model = KPN(
             color=color,
-            burst_length=burst_length,
+            burst_length=1,
             blind_est=True,
             kernel_size=[5],
             sep_conv=False,
@@ -337,66 +329,91 @@ def eval(args):
     else:
         print(" Model type not valid")
         return
-    if args.cuda:
-        model = model.cuda()
-
-    if args.mGPU:
-        model = nn.DataParallel(model)
+    checkpoint_dir = "checkpoints/" + args.checkpoint
+    if not os.path.exists(checkpoint_dir) or len(os.listdir(checkpoint_dir)) == 0:
+        print('There is no any checkpoint file in path:{}'.format(checkpoint_dir))
     # load trained model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ckpt = load_checkpoint(checkpoint_dir,cuda=device=='cuda',best_or_latest=args.load_type)
-    model.load_state_dict(ckpt['state_dict'])
+    state_dict = ckpt['state_dict']
+
+    # if not args.cuda:
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    model.load_state_dict(new_state_dict)
+    # else:
+    #     model.load_state_dict(ckpt['state_dict'])
+
+    model.to(device)
     print('The model has been loaded from epoch {}, n_iter {}.'.format(ckpt['epoch'], ckpt['global_iter']))
     # switch the eval mode
     model.eval()
-
-    # data_loader = iter(data_loader)
+    # model= save_dict['state_dict']
     trans = transforms.ToPILImage()
-
-    with torch.no_grad():
-        psnr = 0.0
-        ssim = 0.0
-        for i, (burst_noise, gt) in enumerate(data_loader):
-            if i < 100:
-                # data = next(data_loader)
-                if args.cuda:
-                    burst_noise = burst_noise.cuda()
-                    gt = gt.cuda()
-                if color:
-                    b, N, c, h, w = burst_noise.size()
-                    feedData = burst_noise.view(b, -1, h, w)
-                else:
-                    feedData = burst_noise
-                pred_i, pred = model(feedData, burst_noise[:, 0:burst_length, ...])
-
-
-                if not color:
-                    psnr_t = calculate_psnr(pred.unsqueeze(1), gt.unsqueeze(1))
-                    ssim_t = calculate_ssim(pred.unsqueeze(1), gt.unsqueeze(1))
-                    psnr_noisy = calculate_psnr(burst_noise[:, 0, ...].unsqueeze(1), gt.unsqueeze(1))
-                else:
-                    psnr_t = calculate_psnr(pred, gt)
-                    ssim_t = calculate_ssim(pred, gt)
-                    psnr_noisy = calculate_psnr(burst_noise[:, 0, ...], gt)
-
-                psnr += psnr_t
-                ssim += ssim_t
-
-                pred = torch.clamp(pred, 0.0, 1.0)
-
-                if args.cuda:
-                    pred = pred.cpu()
-                    gt = gt.cpu()
-                    burst_noise = burst_noise.cpu()
-
-                trans(burst_noise[0, 0, ...].squeeze()).save(os.path.join(eval_dir, '{}_noisy_{:.2f}dB.png'.format(i, psnr_noisy)), quality=100)
-                trans(pred.squeeze()).save(os.path.join(eval_dir, '{}_pred_{:.2f}dB.png'.format(i, psnr_t)), quality=100)
-                trans(gt.squeeze()).save(os.path.join(eval_dir, '{}_gt.png'.format(i)), quality=100)
-
-                print('{}-th image is OK, with PSNR: {:.2f}dB, SSIM: {:.4f}'.format(i, psnr_t, ssim_t))
-            else:
-                break
-        # print('All images are OK, average PSNR: {:.2f}dB, SSIM: {:.4f}'.format(psnr/100, ssim/100))
+    torch.manual_seed(0)
+    all_noisy_imgs = scipy.io.loadmat(args.noise_dir)['siddplus_valid_noisy_srgb']
+    all_clean_imgs = scipy.io.loadmat(args.gt_dir)['siddplus_valid_gt_srgb']
+    i_imgs,_,_,_ = all_noisy_imgs.shape
+    psnrs = []
+    ssims = []
+    for i_img in range(i_imgs):
+        image_noise = transforms.ToTensor()(Image.fromarray(all_noisy_imgs[i_img]))
+        image_noise,image_noise_hr = load_data(image_noise,burst_length)
+        image_noise_hr = image_noise_hr.to(device)
+        # begin = time.time()
+        image_noise_batch = image_noise.to(device)
+        # print(image_noise_batch.size())
+        burst_size = image_noise_batch.size()[1]
+        burst_noise = image_noise_batch.to(device)
+        # print(burst_noise.size())
+        # print(image_noise_hr.size())
+        if color:
+            b, N, c, h, w = burst_noise.size()
+            feedData = burst_noise.view(b, -1, h, w)
+        else:
+            feedData = burst_noise
+        # print(feedData.size())
+        pred = model(feedData, burst_noise[:, 0:burst_length, ...],image_noise_hr)
+        pred = pred.detach().cpu()
+        # print("Time : ", time.time()-begin)
+        gt = transforms.ToTensor()(Image.fromarray(all_clean_imgs[i_img]))
+        gt = gt.unsqueeze(0)
+        # print(pred_i.size())
+        # print(pred[0].size())
+        psnr_t = calculate_psnr(pred, gt)
+        ssim_t = calculate_ssim(pred, gt)
+        psnrs.append(psnr_t)
+        ssims.append(ssim_t)
+        print(i_img,"   UP   :  PSNR : ", str(psnr_t)," :  SSIM : ", str(ssim_t))
+        if args.save_img != '':
+            if not os.path.exists(args.save_img):
+                os.makedirs(args.save_img)
+            plt.figure(figsize=(15, 15))
+            plt.imshow(np.array(trans(pred[0])))
+            plt.title("denoise KPN DGF "+args.model_type, fontsize=25)
+            image_name = str(i_img)
+            plt.axis("off")
+            plt.suptitle(image_name+"   UP   :  PSNR : "+ str(psnr_t)+" :  SSIM : "+ str(ssim_t), fontsize=25)
+            plt.savefig( os.path.join(args.save_img,image_name + "_" + args.checkpoint + '.png'),pad_inches=0)
+        """
+        if args.save_img:
+            # print(np.array(trans(mf8[0])))
+            plt.figure(figsize=(30, 9))
+            plt.subplot(1,3,1)
+            plt.imshow(np.array(trans(pred[0])))
+            plt.title("denoise DGF "+args.model_type, fontsize=26)
+            plt.subplot(1,3,2)
+            plt.imshow(np.array(trans(gt[0])))
+            plt.title("gt ", fontsize=26)
+            plt.subplot(1,3,3)
+            plt.imshow(np.array(trans(image_noise_hr[0])))
+            plt.title("noise ", fontsize=26)
+            plt.axis("off")
+            plt.suptitle(str(i)+"   UP   :  PSNR : "+ str(psnr_t)+" :  SSIM : "+ str(ssim_t), fontsize=26)
+            plt.savefig("checkpoints/22_DGF_" + args.checkpoint+str(i)+'.png',pad_inches=0)
+        """
+    print("   AVG   :  PSNR : "+ str(np.mean(psnrs))+" :  SSIM : "+ str(np.mean(ssims)))
 
 
 if __name__ == '__main__':
