e52a715f1d025fdff4467e6df746572f9ccd5a83
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-12-18 10:02:38 +0700

    add highwave2

---
+++ model/NonKPN_highwave2.py
@@ -0,0 +1,159 @@
+from model.KPN import *
+from utils.wavelet_highwave import DWT_highwave,IWT_highwave
+from utils.wavelet import DWT,IWT
+
+class retruct_basic_low(nn.Module):
+    def __init__(self, in_channel):
+        super(retruct_basic_low, self).__init__()
+        self.upMode = 'bilinear'
+        channel_att = False
+        spatial_att = False
+        in_channel = in_channel
+
+        out_channel = in_channel
+
+        self.conv1 = Basic(in_channel, 64, channel_att=False, spatial_att=False)
+        self.conv2 = Basic(64, 128, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.conv3 = Basic(128, 256, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.conv4 = Basic(256, 512, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        # 6~8层要先上采样再卷积
+        self.conv7 = Basic(256 + 512, 256, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.conv8 = Basic(256 + 128, 128, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.conv9 = Basic(128+64, 64, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.outc = nn.Sequential(
+            Basic(64, 64),
+            nn.Conv2d(64, out_channel, 1, 1, 0)
+        )
+
+    def forward(self, data):
+        conv1 = self.conv1(data)
+        conv2 = self.conv2(F.avg_pool2d(conv1, kernel_size=2, stride=2))
+        conv3 = self.conv3(F.avg_pool2d(conv2, kernel_size=2, stride=2))
+        conv4 = self.conv4(F.avg_pool2d(conv3, kernel_size=2, stride=2))
+
+        conv7 = self.conv7(torch.cat([conv3, F.interpolate(conv4, scale_factor=2, mode=self.upMode)], dim=1))
+        conv8 = self.conv8(torch.cat([conv2, F.interpolate(conv7, scale_factor=2, mode=self.upMode)], dim=1))
+        conv9 = self.conv9(torch.cat([conv1, F.interpolate(conv8, scale_factor=2, mode=self.upMode)], dim=1))
+        # return channel K*K*N
+        out = self.outc(conv9)
+        return out
+class retruct_basic_high(nn.Module):
+    def __init__(self, in_channel):
+        super(retruct_basic_high, self).__init__()
+        self.upMode = 'bilinear'
+        channel_att = False
+        spatial_att = False
+        in_channel = in_channel
+
+        out_channel = in_channel
+
+        self.conv1 = Basic(in_channel, 64, channel_att=False, spatial_att=False,bn=True)
+        self.conv2 = Basic(64, 128, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.conv3 = Basic(128, 256, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+        self.conv4 = Basic(256, 256, channel_att=channel_att, spatial_att=spatial_att,bn=True)
+
+        self.outc = nn.Sequential(
+            Basic(256, 64, channel_att=channel_att, spatial_att=spatial_att,bn=True),
+            Basic(64, 64),
+            nn.Conv2d(64, out_channel, 1, 1, 0)
+        )
+
+    def forward(self, data):
+        conv1 = self.conv1(data)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+
+        out = self.outc(conv4)
+        return out
+
+class Att_NonKPN_Wavelet_highwave2(nn.Module):
+    def __init__(self, color=True, burst_length=8, channel_att=False, spatial_att=False, upMode='bilinear', core_bias=False):
+        super(Att_NonKPN_Wavelet_highwave2, self).__init__()
+        self.upMode = upMode
+        self.burst_length = burst_length
+        self.core_bias = core_bias
+        self.color_channel = 3 if color else 1
+        # in_channel = (3 if color else 1) * (burst_length if blind_est else burst_length + 1)
+        in_channel = (3 if color else 1)
+
+        out_channel = (3 if color else 1)
+        self.DWT = DWT_highwave()
+        self.IWT = IWT_highwave()
+        # 各个卷积层定义
+        # 2~5 Down image
+        n_feat = 64
+        # self.conv1 = Basic(in_channel, n_feat, channel_att=channel_att, spatial_att=spatial_att)
+        print(in_channel)
+        self.low_1 = retruct_basic_low(in_channel)
+        self.high_hh1 = retruct_basic_high(in_channel)
+        self.high_hl1 = retruct_basic_high(in_channel)
+        self.high_lh1 = retruct_basic_high(in_channel)
+        self.low_2 = retruct_basic_low(in_channel)
+        self.high_hh2 = retruct_basic_high(in_channel)
+        self.high_hl2 = retruct_basic_high(in_channel)
+        self.high_lh2 = retruct_basic_high(in_channel)
+        self.low_3 = retruct_basic_low(in_channel)
+        self.high_hh3 = retruct_basic_high(in_channel)
+        self.high_lh3 = retruct_basic_high(in_channel)
+        self.high_hl3 = retruct_basic_high(in_channel)
+
+        self.outc = nn.Sequential(
+            Basic(in_channel, 64, channel_att=channel_att, spatial_att=spatial_att),
+            Basic(64, 64),
+            nn.Conv2d(64, out_channel, 1, 1, 0)
+        )
+        self.apply(self._init_weights)
+
+    @staticmethod
+    def _init_weights(m):
+        if isinstance(m, nn.Conv2d):
+            nn.init.xavier_normal_(m.weight.data)
+            if type(m.bias) != type(None):
+                nn.init.constant_(m.bias.data, 0.0)
+        elif isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight.data)
+            nn.init.constant_(m.bias.data, 0.0)
+
+    # 前向传播函数
+    def forward(self, data):
+        """
+        forward and obtain pred image directly
+        :param data_with_est: if not blind estimation, it is same as data
+        :param data:
+        :return: pred_img_i and img_pred
+        """
+        x_LL1, x_HL1, x_LH1, x_HH1 = self.DWT(data)
+        x_HL1 = self.high_hl1(x_HL1)
+        x_HH1 = self.high_hh1(x_HH1)
+        x_LH1 = self.high_lh1(x_LH1)
+
+        x_LL2, x_HL2, x_LH2, x_HH2 = self.DWT(x_LL1)
+        x_HL2 = self.high_hl1(x_HL2)
+        x_HH2 = self.high_hh1(x_HH2)
+        x_LH2 = self.high_lh1(x_LH2)
+
+        x_LL3, x_HL3, x_LH3, x_HH3 = self.DWT(x_LL2)
+        x_HL3 = self.high_hl1(x_HL3)
+        x_HH3 = self.high_hh1(x_HH3)
+        x_LH3 = self.high_lh1(x_LH3)
+        print("x_LH3 : ",x_H3)
+        x_LL3 = self.low_3(x_LL3)
+        x_LL2 = self.IWT(torch.cat([x_LL3, x_HL3, x_LH3, x_HH3],dim=1))
+        x_LL2 = self.low_2(x_LL2)
+        x_LL1 = self.IWT(torch.cat([x_LL2, x_HL2, x_LH2, x_HH2],dim=1))
+        x_LL1 = self.low_1(x_LL1)
+        x = self.IWT(torch.cat([x_LL1, x_HL1, x_LH1, x_HH1],dim=1))
+        out = self.outc(x)
+        return out
+
+if __name__ == "__main__":
+    model = Att_NonKPN_Wavelet_highwave2(
+        color=True,
+        burst_length=1,
+        channel_att=True,
+        spatial_att=True,
+        upMode="bilinear",
+        core_bias=False,
+    )
+    print(model)
--- utils/wavelet_highwave.py
+++ utils/wavelet_highwave.py
@@ -6,17 +6,40 @@ def dwt_init(x):
 
     x01 = x[:, :, 0::2, :] / 2
     x02 = x[:, :, 1::2, :] / 2
-    x1 = x01[:, :, :, 0::2]
-    x2 = x02[:, :, :, 0::2]
-    x3 = x01[:, :, :, 1::2]
-    x4 = x02[:, :, :, 1::2]
+    x1 = x01[:, :, :, 0::2] /2
+    x2 = x02[:, :, :, 0::2] /2
+    x3 = x01[:, :, :, 1::2] /2
+    x4 = x02[:, :, :, 1::2] /2
     x_LL = x1 + x2 + x3 + x4
     x_HL = -x1 - x2 + x3 + x4
     x_LH = -x1 + x2 - x3 + x4
     x_HH = x1 - x2 - x3 + x4
 
-    return x_LL, torch.cat((x_HL, x_LH, x_HH), 1)
+    return x_LL, x_HL, x_LH, x_HH
 
+
+
+def iwt_init(x):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    r = 2
+    in_batch, in_channel, in_height, in_width = x.size()
+    # print([in_batch, in_channel, in_height, in_width])
+    out_batch, out_channel, out_height, out_width = in_batch, int(
+        in_channel / (r ** 2)), r * in_height, r * in_width
+    x1 = x[:, 0:out_channel, :, :]
+    x2 = x[:, out_channel:out_channel * 2, :, :]
+    x3 = x[:, out_channel * 2:out_channel * 3, :, :]
+    x4 = x[:, out_channel * 3:out_channel * 4, :, :]
+
+
+    h = torch.zeros([out_batch, out_channel, out_height, out_width]).float().to(device)
+
+    h[:, :, 0::2, 0::2] = x1 - x2 - x3 + x4
+    h[:, :, 1::2, 0::2] = x1 - x2 + x3 - x4
+    h[:, :, 0::2, 1::2] = x1 + x2 - x3 - x4
+    h[:, :, 1::2, 1::2] = x1 + x2 + x3 + x4
+
+    return h
 
 class DWT_highwave(nn.Module):
     def __init__(self):
@@ -25,3 +48,11 @@ class DWT_highwave(nn.Module):
 
     def forward(self, x):
         return dwt_init(x)
+
+class IWT_highwave(nn.Module):
+    def __init__(self):
+        super(IWT_highwave, self).__init__()
+        self.requires_grad = False
+
+    def forward(self, x):
+        return iwt_init(x)
