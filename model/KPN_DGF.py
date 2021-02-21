6731e207b49b7dcb64f7e862c14ab29859d8107b
Phạm Minh Tâm <pminhtamnb@gmail.com>
2021-02-21 09:32:58 +0700

    gix giuded filter

--- model/KPN_DGF.py
+++ model/KPN_DGF.py
@@ -2,7 +2,7 @@ from model.KPN import KPN
 from model.Att_KPN import Att_KPN,Att_KPN_Wavelet
 from model.Att_Weight_KPN import Att_Weight_KPN
 import torch.nn as nn
-from guided_filter_pytorch.guided_filter import ConvGuidedFilter
+from guided_filter_pytorch.guided_filter import ConvGuidedFilter2
 
 class KPN_DGF(nn.Module):
     def __init__(self,color=True, burst_length=8, blind_est=False, kernel_size=[5], sep_conv=False,
@@ -19,7 +19,7 @@ class KPN_DGF(nn.Module):
             upMode=upMode,
             core_bias=core_bias
         )
-        self.gf = ConvGuidedFilter(radius=1)
+        self.gf = ConvGuidedFilter2(radius=1)
 
     def forward(self,data_with_est, data,x_hr):
         pred_i, pred = self.KPN(data_with_est, data)
@@ -29,7 +29,6 @@ class KPN_DGF(nn.Module):
         # print(data[:,0,:,:,:].size())
         data_feed = data[:,0,:,:,:].view(-1,c, h, w)
         pred_feed = pred.view(-1,c, h, w)
-        pred_i = pred_i.view(-1,c*N,h,w)
 
         b_hr, c_hr, h_hr, w_hr = x_hr.size()
         # print("x_hr  ",x_hr.size())
@@ -37,8 +36,7 @@ class KPN_DGF(nn.Module):
         # print(data_feed.size())
         # print(pred_feed.size())
         # print(x_hr_feed.size())
-        # out_hr = self.gf(data.view(b,c*N, h, w), pred_i, x_hr_feed)
-        out_hr = self.gf(data.view(b,c*N, h, w), pred_feed.repeat(1,N, 1, 1), x_hr_feed)
+        out_hr = self.gf(data_feed, pred_feed, x_hr_feed)
 
         out_hr = out_hr.view(b,c, h_hr,w_hr)
         return pred_i,out_hr
@@ -58,7 +56,7 @@ class Att_KPN_DGF(nn.Module):
             upMode=upMode,
             core_bias=core_bias
         )
-        self.gf = ConvGuidedFilter(radius=1)
+        self.gf = ConvGuidedFilter2(radius=1)
 
     def forward(self,data_with_est, data,x_hr):
         pred_i, pred = self.Att_KPN(data_with_est, data)
@@ -68,15 +66,17 @@ class Att_KPN_DGF(nn.Module):
         # print(data[:,0,:,:,:].size())
         data_feed = data[:,0,:,:,:].view(-1,c, h, w)
         pred_feed = pred.view(-1,c, h, w)
-
+        # pred_i = pred_i.view(-1,c*N,h,w)
         b_hr, c_hr, h_hr, w_hr = x_hr.size()
         # print("x_hr  ",x_hr.size())
         x_hr_feed = x_hr.view(-1,c, h_hr, w_hr)
         # print(data_feed.size())
-        # print(pred_feed.size())
+        # print(pred_feed.repeat(1,N, 1, 1).size())
+        # print(pred_i.size())
         # print(x_hr_feed.size())
-        out_hr = self.gf(data_feed, pred_feed, x_hr_feed)
-
+        # out_hr = self.gf(data.view(b,c*N, h, w), pred_i.view(-1,c*N,h,w), x_hr_feed)
+        out_hr = self.gf(data.view(b,c*N, h, w), pred_feed.repeat(1,N, 1, 1), x_hr_feed)
+        # print(out_hr.shape)
         out_hr = out_hr.view(b,c, h_hr,w_hr)
         return pred_i,out_hr
 
@@ -95,7 +95,7 @@ class Att_Weight_KPN_DGF(nn.Module):
             upMode=upMode,
             core_bias=core_bias
         )
-        self.gf = ConvGuidedFilter(radius=1)
+        self.gf = ConvGuidedFilter2(radius=1)
 
     def forward(self,data_with_est, data,x_hr):
         pred_i, pred = self.Att_Weight_KPN(data_with_est, data)
@@ -132,7 +132,7 @@ class Att_KPN_Wavelet_DGF(nn.Module):
             upMode=upMode,
             core_bias=core_bias
         )
-        self.gf = ConvGuidedFilter(radius=1)
+        self.gf = ConvGuidedFilter2(radius=1)
 
     def forward(self,data_with_est, data,x_hr):
         pred_i, pred = self.Att_KPN_Wavelet(data_with_est, data)
